48c972b2b46d9c5282c02c8ccf63f87af5cba662
Fabirc98 <frcorte@sudamericano.edu.ec>
2023-11-19 18:10:24 -0500

    .js

---
+++ edad.js
@@ -0,0 +1,10 @@
+'use strict'
+
+
+let anioNacimiento = Number(prompt("ingrese año de nacimiento"));
+function mostraredad() {
+   let anioactual = 2023 ;
+   let edad = anioactual- anioNacimiento;
+   alert("la edad es"+ edad + "años");
+}
+mostraredad();
